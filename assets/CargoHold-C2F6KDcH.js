fc4ca16169d8f90d1e850d11534eb1f89b907f8a
Ruadhan2301 <49420750+Ruadhan2301@users.noreply.github.com>
2025-01-07 20:17:55 +0000

    Include Dist folder for ease of deployment

---
+++ assets/CargoHold-C2F6KDcH.js
@@ -0,0 +1,1 @@
+import{d as e,j as o,k as t,u as s,a as c,o as n,_ as r}from"./index-jo8d_dci.js";const _=e({__name:"CargoHold",setup(p){const a=o();return o(),t(()=>{a.value=s().contracts}),(d,u)=>(n(),c("main"))}}),m=r(_,[["__scopeId","data-v-237ae9c8"]]);export{m as default};
