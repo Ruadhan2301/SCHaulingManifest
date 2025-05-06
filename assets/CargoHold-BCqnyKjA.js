b7d55a62498fa93320e21c2b0e0a7dd1a7d4f20b
Ruadhan2301 <49420750+Ruadhan2301@users.noreply.github.com>
2025-05-06 15:52:51 +0100

    Delete assets directory

--- assets/CargoHold-BCqnyKjA.js
+++
@@ -1,1 +0,0 @@
-import{d as t,j as e,k as a,u as n,a as r,o as s}from"./index-CwrKe7WW.js";const p=t({__name:"CargoHold",setup(c){const o=e();return e(),a(()=>{o.value=n().contracts}),(u,_)=>(s(),r("main"))}});export{p as default};
