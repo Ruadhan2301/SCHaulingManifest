3509753f5ee05bdff2c9de161f29086418683c16
Ruadhan2301 <49420750+Ruadhan2301@users.noreply.github.com>
2025-05-06 15:47:35 +0100

    Delete assets directory

--- assets/CargoHold-BFpvrmox.js
+++
@@ -1,1 +0,0 @@
-import{d as t,j as e,k as a,u as n,a as r,o as s}from"./index-C2fs90MC.js";const p=t({__name:"CargoHold",setup(c){const o=e();return e(),a(()=>{o.value=n().contracts}),(u,_)=>(s(),r("main"))}});export{p as default};
